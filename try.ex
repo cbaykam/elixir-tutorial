369b5cc51b1bff1353ef601cb7647a5a7fec856a
Metebykm <metebaykam.final01@gmail.com>
2015-06-27 11:03:22 +0300

    greeter fonksiyonu yazildi

--- try.ex
+++ try.ex
@@ -1,1 +1,9 @@
-IO.puts "Hello \n\n\n World"
+greet = fn
+	greeter, {:person, first_name, last_name} -> greeter.(first_name <> " " <> last_name)
+end
+
+polite_greeter = fn 
+	name -> IO.puts "hello " <> name
+end
+
+greet.(polite_greeter, {:person, "METE", "BAYKAM"})
